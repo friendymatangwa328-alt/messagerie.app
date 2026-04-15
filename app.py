5923e3e478929b38d7f910587f9d0b955079a96d
friendymatangwa328-alt <friendymatangwa328@gmail.com>
2026-04-15 09:54:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
-# Page d'accueil → login
+# Page d'accueil → redirige vers login
 @app.route('/')
 def home():
-    return render_template('login.html')
+    return redirect('/login')
 # Page login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        # simple vérification (tu peux améliorer après)
-        if username == "admin" and password == "1234":
-            return redirect(url_for('chat'))
-        else:
-            return "Erreur connexion"
+        return redirect('/chat')
     return render_template('login.html')
 # Page chat
 @app.route('/chat')
@@ -24,10 +18,9 @@ def chat():
 @app.route('/interface')
 def interface():
     return render_template('interface.html')
-# Page users
+# Page utilisateurs
 @app.route('/users')
 def users():
     return render_template('users.html')
-# Lancer serveur
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
